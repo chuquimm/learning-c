570b0e2dc7ca9a9a155482244cce1b0954ad71f4
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2021-01-02 23:05:17 -0500

    Hello World

---
+++ hello.c
@@ -0,0 +1,15 @@
+// Preprocessor commands
+#include <stdio.h>
+
+// The main function serves as the starting point for program execution
+int main()
+{
+    printf("Hello world!\n");
+    return 0; // finish the main function
+}
+
+/*
+To compile:
+gcc -o hello hello.c
+./hello
+*/
